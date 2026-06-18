30929daa2a089acf9bf98d2ad71936122c90514f
madmext <info@madmext.com>
2026-06-18 23:20:28 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,10 @@
-from app import app
+from app import app, get_db, hash_pw, read_logs, write_logs
+import password_reset_flow
+
+password_reset_flow.install(
+    app,
+    get_db=get_db,
+    hash_pw=hash_pw,
+    read_logs=read_logs,
+    write_logs=write_logs
+)
