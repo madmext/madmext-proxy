38bfe9af603189e8507fc2e100008174e4b03c86
madmext <info@madmext.com>
2026-07-11 13:14:39 +0300

    Install OneSignal notification routes

--- entrypoint.py
+++ entrypoint.py
@@ -2,7 +2,8 @@ import os
 from flask import jsonify, send_from_directory, session, request, Response
 from server import app
 import meta_sync_flow
-from app import get_db, read_logs, write_logs
+import onesignal_flow
+from app import get_db, read_logs, write_logs, require_admin
 
 
 meta_sync_flow.install(
@@ -10,6 +11,12 @@ meta_sync_flow.install(
     get_db=get_db,
     read_logs=read_logs,
     write_logs=write_logs,
+)
+
+onesignal_flow.install(
+    app,
+    get_db=get_db,
+    require_admin=require_admin,
 )
 
 
@@ -40,6 +47,7 @@ _RESERVED_PREFIXES = (
     'claude',
     'proxy-xml',
     'trendyol',
+    'onesignal',
 )
 
 
