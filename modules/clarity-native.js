e2f0aec17394fccde8bc4fe9e5a9a35455dfed55
madmext <info@madmext.com>
2026-07-17 15:15:11 +0300

    Add native Clarity navigation bootstrap

---
+++ modules/clarity-native.js
@@ -0,0 +1,14 @@
+(function(){
+  'use strict';
+
+  var PAGE_ID='clarity-analizi';
+  var SECTIONS=[
+    ['▦','Genel Bakış',PAGE_ID,'overview'],
+    ['👆','Etkileşim Sorunları',PAGE_ID,'friction'],
+    ['📄','Sayfa & URL Analizi',PAGE_ID,'pages'],
+    ['📣','Trafik Kaynakları',PAGE_ID,'traffic'],
+    ['📱','Cihaz & Teknoloji',PAGE_ID,'technology'],
+    ['🌍','Ülke & Bölge',PAGE_ID,'geography'],
+    ['🎯','Kampanya Analizi',PAGE_ID,'campaigns'],
+    ['🧪','Ham Veri Gezgini',PAGE_ID,'raw'],
+    ['↻','Senkronizasyon',PAGE_ID
